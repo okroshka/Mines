bad98aafdf8c4375609d7edb798cd552eec78d0f
Alexandr Akulich <akulichalexander@gmail.com>
2016-03-10 14:43:50 +0500

    CellItem: Implemented minesAround rendering.
    Closes #14.

--- CellItem.cpp
+++ CellItem.cpp
@@ -35,6 +35,8 @@ void CellItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, 
 
     if (m_cell->haveMine()) {
         m_text->setText("+");
+    } else if (m_cell->minesAround() > 0) {
+        m_text->setText(QString::number(m_cell->minesAround()));
     }
 }
 
